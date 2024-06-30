a28b7bdc779f0e93b8338b6f9ce717a906efe873
Mateusz <dsw52746@student.dsw.edu.pl>
2024-06-30 21:25:57 +0200

    Upgrade skryptu

--- skrypt.sh
+++ skrypt.sh
@@ -1,26 +1,59 @@
 #!/bin/bash
 
-if [[ $1 == "--date" ]]; then
-  date
-elif [[ $1 == "--logs" ]]; then
-  echo -n "Logi do utworzenia: "
-  read num_logs
+echo ".gitignore" > .gitignore
 
-  # Validate user input (optional)
-  if ! [[ "$num_logs" =~ ^[1-9][0-9]*$ ]]; then
-    echo "Nie mozna utworzyc mniej niz 0 logow."
+if [[ $1 == "--date" || $1 == "-d" ]]; then
+  date
+fi
+
+if [[ $1 == "--logs" || $1 == "-l" ]]; then
+  if [[ $2 =~ ^[1-9][0-9]*$ ]]; then
+    num_logs=$2
+  else
+    echo "Nieprawidłowy argument: Nie można utworzyć mniej niż 0 logów."
     exit 1
   fi
 
   for (( i=1; i<=$num_logs; i++ )); do
     echo "Log numer $i stworzony przez $0" > "log_$i.txt"
   done
-elif [[ $1 == "--help" ]]; then
-  echo "Użycie: ./skrypt.sh [Opcje]"
-  echo "Opcje:"
-  echo "  --date	Aktualna data"
-  echo "  --logs	Produkuje wybraną liczbę logów"
-  echo "  --help	Wyświetla tą wiadomość"
-else
-  echo "Zly argument - użyj --help"
 fi
+
+if [[ $1 == "--help" || $1 == "-h" ]]; then
+  echo "Użycie: $0 [argument]"
+  echo "Argumenty:"
+  echo "  -d, --date     Wyświetla aktualną datę"
+  echo "  -l, --logs N   Tworzy logi"
+  echo "  -h, --help     Wyświetla tę wiadomość"
+  exit 0
+fi
+
+while getopts ":hd:l:" opt; do
+  case $opt in
+    h)
+      echo "Sposób użycia: $0 [OPCJE]"
+      echo "Opcje:"
+      echo "  -d       Wyświetla aktualną datę"
+      echo "  -l       Tworzy logi"
+      echo "  -h       Wyświetla tę wiadomość"
+      exit 0
+      ;;
+    d)
+      date
+      ;;
+    l)
+      num_logs=$OPTARG
+      if ! [[ "$num_logs" =~ ^[1-9][0-9]*$ ]]; then
+        echo "Nieprawidłowy argument: Nie można utworzyć mniej niż 0 logów."
+        exit 1
+      fi
+      for (( i=1; i<=$num_logs; i++ )); do
+        echo "Log numer $i stworzony przez $0" > "log_$i.txt"
+      done
+      ;;
+    \?)
+      echo "Nieprawidłowy argument -$OPTARG. Użyj -h aby uzyskać pomoc."
+      exit 1
+      ;;
+  esac
+done
